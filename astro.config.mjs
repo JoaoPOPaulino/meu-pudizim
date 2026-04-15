5ef1f80e0f49bc1f101aeb0e0d7d85a60901beea
João Pedro de Oliveira Paulino <joaopoliveirapaulino@gmail.com>
2026-04-15 13:14:14 -0300

    Fix vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
-import node from '@astrojs/node';
-
-// https://astro.build/config
 export default defineConfig({
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: vercel()
 });
