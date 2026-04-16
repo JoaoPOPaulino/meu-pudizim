6a814dd46f776adae01f14a08b338e50e9e5f70c
João Pedro de Oliveira Paulino <joaopoliveirapaulino@gmail.com>
2026-04-16 10:07:36 -0300

    style: Atualização nos sabores e dropdown para a seleção

--- src/data/site.js
+++ src/data/site.js
@@ -4,25 +4,37 @@ export const site = {
   cidade: "Canoas - RS",
   mensagem: "Olá! Vim pelo site e quero fazer um pedido.",
   instagram: "https://www.instagram.com/meupudizim/",
-  instaPersoal:'https://www.instagram.com/karineollins/'
+  instaPersoal: 'https://www.instagram.com/karineollins/'
 }
+
+export const sabores = [
+  "Tradicional",
+  "Doce de Leite",
+  "Chocolate Branco com Calda de Morango",
+  "Cream Cheese com Goiabada",
+  "Cream Cheese Caramelizado",
+]
 
 export const cardapio = [
   {
-    nome: "Pudim Clássico",
-    descricao: "O tradicional pudim de leite condensado com calda de caramelo dourado. Cremoso, irresistível e feito na hora.",
-    preco: "00,00",
-    destaque: true,
-  }
-]
-
-export const depoimentos = [
-  {
-    texto: "Melhor pudim que já comi na vida! A calda é perfeita e o pudim derrete na boca.",
-    autor: "Maria S.",
+    nome: "Pudim Individual",
+    tamanho: "120g",
+    descricao: "Porção individual perfeita para matar a vontade. Escolha o seu sabor favorito!",
+    preco: "12,00",
+    destaque: false,
   },
   {
-    texto: "Pedi para o aniversário da minha filha e foi um sucesso. Todo mundo perguntou onde comprei!",
-    autor: "Joana R.",
-  }
+    nome: "Pudim Médio",
+    tamanho: "600g",
+    descricao: "Tamanho ideal para compartilhar com a família ou amigos. Escolha o seu sabor favorito!",
+    preco: "50,00",
+    destaque: true,
+  },
+  {
+    nome: "Pudim Família",
+    tamanho: "1,1kg",
+    descricao: "O maior e mais generoso! Para celebrar em grande estilo. Escolha o seu sabor favorito!",
+    preco: "80,00",
+    destaque: false,
+  },
 ]
